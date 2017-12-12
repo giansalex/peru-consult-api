c714a028d8ff65958adf22b98d20ab14840ff3ca
giansalex <giansalex@gmail.com>
2017-12-12 16:00:56 -0500

    add route consult & dependencies

--- src/dependencies.php
+++ src/dependencies.php
@@ -1,6 +1,9 @@
 <?php
 // DIC configuration
 
+use Peru\Reniec\Dni;
+use Peru\Sunat\Ruc;
+
 $container = $app->getContainer();
 
 // view renderer
@@ -14,4 +17,13 @@ $container['logger'] = function ($c) {
     $settings = $c->get('settings')['logger'];
     $logger =  new Katzgrau\KLogger\Logger($settings['path'], $settings['level'], ['extension' => 'log']);
     return $logger;
+};
+
+
+$container[Ruc::class] = function () {
+    return new Ruc();
+};
+
+$container[Dni::class] = function () {
+    return new Dni();
 };
--- src/routes.php
+++ src/routes.php
@@ -1,14 +1,45 @@
 <?php
 
+use Peru\Reniec\Dni;
+use Peru\Sunat\Ruc;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->group('/api', function () {
+    /**@var $this Slim\App */
+    $this->get('/ruc/{ruc:\d{11}}', function (Request $request, Response $response, array $args) {
+        $ruc = $args['ruc'];
+        /**@var $service Ruc */
+        $service = $this->get(Ruc::class);
+        $company = $service->get($ruc);
+        if ($company === false) {
+            $this->logger->error($service->getError());
+            $response->getBody()->write($service->getError());
+            return $response->withStatus(500);
+        }
+
+        return $response->withJson(get_object_vars($company));
+    });
+
+    $this->get('/dni/{dni:\d{8}}', function (Request $request, Response $response, array $args) {
+        $dni = $args['dni'];
+        /**@var $service Dni */
+        $service = $this->get(Dni::class);
+        $person = $service->get($dni);
+        if ($person === false) {
+            $this->logger->error($service->getError());
+            $response->getBody()->write($service->getError());
+            return $response->withStatus(500);
+        }
+
+        return $response->withJson(get_object_vars($person));
+    });
+});
+$app->get('/', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
-    // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
