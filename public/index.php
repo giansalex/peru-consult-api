2238c3fdeb5be6d9605681e6bf8a64e9169d61b1
giansalex <giansalex@gmail.com>
2017-12-12 15:44:28 -0500

    update description composer, set index-dev

--- public/index.php
+++ public/index.php
@@ -1,30 +1,13 @@
 <?php
-if (PHP_SAPI == 'cli-server') {
-    // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
-    $url  = parse_url($_SERVER['REQUEST_URI']);
-    $file = __DIR__ . $url['path'];
-    if (is_file($file)) {
-        return false;
-    }
-}
+$dir = __DIR__;
+require $dir . '/../vendor/autoload.php';
 
-require __DIR__ . '/../vendor/autoload.php';
-
-session_start();
-
-// Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
+$settings = require $dir . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
-// Set up dependencies
-require __DIR__ . '/../src/dependencies.php';
-
-// Register middleware
-require __DIR__ . '/../src/middleware.php';
-
-// Register routes
-require __DIR__ . '/../src/routes.php';
+require $dir . '/../src/dependencies.php';
+require $dir . '/../src/middleware.php';
+require $dir . '/../src/routes.php';
 
 // Run app
 $app->run();
