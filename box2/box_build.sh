a684c32bc2b405586f14c49c9088fe5658d9d02e
giansalex <giansalex@gmail.com>
2018-01-15 20:38:34 -0500

    change to bash

---
+++ box2/box_build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+cd ..
+rm -rf dist
+cp docker\settings.php src\settings.php
+rm -rf vendor
+composer install --no-dev --optimize-autoloader
+box build
+mkdir dist
+mv consult.phar dist\consult.phar
+mv public\assets dist\assets
+cp public\.htaccess dist\.htaccess
+cp box2\index.php dist\index.php
+git checkout .
