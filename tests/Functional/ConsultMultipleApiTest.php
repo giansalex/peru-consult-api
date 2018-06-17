a2a0e557116a2e111a9e659ba5695f534140409b
Giancarlos Salas <giansalex@gmail.com>
2018-06-16 22:05:45 -0500

    exclude dni from tests

--- tests/Functional/ConsultMultipleApiTest.php
+++ tests/Functional/ConsultMultipleApiTest.php
@@ -45,39 +45,4 @@ class ConsultMultipleApiTest extends BaseTestCase
             $this->assertNotEmpty($company->razonSocial);
         }
     }
-
-    public function testConsultDniNotAllowed()
-    {
-        $response = $this->runApp('GET', '/api/v1/dni');
-
-        $this->assertEquals(405, $response->getStatusCode());
-    }
-
-    public function testConsultDniNotValid()
-    {
-        $response = $this->runApp('POST', '/api/v1/dni');
-
-        $this->assertEquals(400, $response->getStatusCode());
-    }
-
-    public function testConsultDni()
-    {
-        $arr = ['00000012', '00000005', '46658592'];
-        $response = $this->runApp('POST', '/api/v1/dni', $arr);
-
-        echo (string)$response->getBody();
-        $this->assertEquals(200, $response->getStatusCode());
-        /**@var $persons Person[] */
-        $persons = json_decode((string)$response->getBody());
-        if (!empty(getenv('CI'))) {
-            return;
-        }
-
-        $this->assertEquals(3, count($persons));
-
-        foreach ($persons as $person) {
-            $this->assertTrue(in_array($person->dni, $arr));
-            $this->assertNotEmpty($person->nombres);
-        }
-    }
 }
