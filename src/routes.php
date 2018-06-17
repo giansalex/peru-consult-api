c7738850c0468620d6a52846ef646e8d11ede070
Giancarlos Salas <giansalex@gmail.com>
2018-06-16 22:02:47 -0500

    remove dni from routes

--- src/routes.php
+++ src/routes.php
@@ -6,9 +6,7 @@ $container = $app->getContainer();
 $app->group('/api/v1', function () {
     /* @var $this Slim\App */
     $this->get('/ruc/{ruc:\d{11}}', 'Peru\Api\Controller\ConsultController:ruc');
-    $this->get('/dni/{dni:\d{8}}', 'Peru\Api\Controller\ConsultController:dni');
     $this->post('/ruc', 'Peru\Api\Controller\ConsultMultipleController:ruc');
-    $this->post('/dni', 'Peru\Api\Controller\ConsultMultipleController:dni');
     $this->get('/user-sol/{ruc}/{user}', 'Peru\Api\Controller\ConsultController:userSol');
     $this->post('/graph', 'Peru\Api\Controller\ConsultController:graph');
 })->add(new \Peru\Api\Middleware\TokenMiddleware($container['settings']['auth']['token']));
