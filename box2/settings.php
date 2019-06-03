b295827f2a11a9305e5eb2d7f5292145af42f1ec
Giancarlos Salas <giansalex@gmail.com>
2019-06-02 20:35:04 -0500

    remove swagger 2.0 definition

--- box2/settings.php
+++ box2/settings.php
@@ -14,7 +14,7 @@ return [
             'token' => getenv('API_TOKEN'),
         ],
 
-        'swagger_path' => __DIR__.'/../public/swagger.json',
+        'swagger_path' => __DIR__.'/../public/openapi.json',
 
         // Monolog settings
         'logger' => [
