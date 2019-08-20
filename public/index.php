01a4e31a89f121421340af4b2410fb970a054fae
Giancarlos Salas <giansalex@gmail.com>
2019-08-19 22:00:45 -0500

    save error async

--- public/index.php
+++ public/index.php
@@ -14,12 +14,28 @@ require $dir.'/../src/routes.php';
 $response = $app->run(true);
 
 if ($response instanceof \Peru\Api\Http\AppResponse) {
-    $response->getPromise()->then(function ($response) use ($app) {
-        $app->respond($response);
-    });
-    $app->getContainer()->get(\React\EventLoop\LoopInterface::class)->run();
-
+    promiseProcess($response);
     return;
 }
 
 $app->respond($response);
+
+function promiseProcess(\Peru\Api\Http\AppResponse $response)
+{
+    global $app;
+    $container = $app->getContainer();
+    $response->getPromise()
+        ->then(function ($response) use ($app) {
+            $app->respond($response);
+        }, function (Exception $e) use ($container, $app) {
+            if ($container['settings']['displayErrorDetails']) {
+                var_dump($e);
+                return;
+            }
+
+            $container->get('logger')->error($e->getMessage());
+            $app->respond((new \Slim\Http\Response(500))
+                ->withJson(['message' => 'Ocurrio un error.']));
+        });
+    $container->get(\React\EventLoop\LoopInterface::class)->run();
+}
