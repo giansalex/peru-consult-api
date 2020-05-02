d8324bce8be353bb170fe5067a10918f660fc4c4
Giancarlos Salas <giansalex@gmail.com>
2020-05-02 13:08:41 -0500

    Add async kernel for tests

---
+++ tests/Kernel.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class Kernel extends \App\Kernel
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function handle(Request $request, int $type = HttpKernelInterface::MASTER_REQUEST, bool $catch = true)
+    {
+        $loop = $this->getContainer()->get('reactphp.event_loop');
+        $promise = $this->handleAsync($request);
+        $response = null;
+        $promise->then(function ($result) use (&$response) {
+            $response = $result;
+        });
+        $loop->run();
+
+        return $response;
+    }
+}
