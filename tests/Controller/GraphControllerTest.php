a8ff947085ea4fb29a44172971f307011e4b218e
Giancarlos Salas <giansalex@gmail.com>
2020-05-02 13:08:50 -0500

    Add unit tests

---
+++ tests/Controller/GraphControllerTest.php
@@ -0,0 +1,80 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Functional;
+
+use Peru\Reniec\Person;
+use Peru\Sunat\Company;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class GraphControllerTest extends WebTestCase
+{
+    public function testConsultGraph()
+    {
+        $q = <<<QL
+query { 
+    company(ruc: "20493919271") {
+        ruc
+        razonSocial
+        nombreComercial
+        telefonos
+        tipo
+        estado
+        condicion
+        direccion
+        departamento
+        provincia
+        distrito
+        fechaInscripcion
+        sistEmsion
+        sistContabilidad
+        actExterior
+        actEconomicas
+        cpPago
+        sistElectronica
+        fechaEmisorFe
+        cpeElectronico
+        fechaPle
+        padrones
+        fechaBaja
+        profesion
+    }
+    person(dni: "48004836") {
+        dni
+        nombres
+    }
+}
+
+QL;
+        $client = static::createClient();
+
+        $client->request(
+            'POST',
+            '/api/graph?token=abcxyz',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode(['query' => $q])
+        );
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $obj = json_decode($client->getResponse()->getContent());
+
+        /**@var $company Company */
+        $company = $obj->data->company;
+        /**@var $person Person */
+        $person = $obj->data->person;
+
+        $this->assertFalse(isset($obj->errors));
+        $this->assertStringContainsString('EMPRESA CONSTRUCTORA', $company->razonSocial);
+        $this->assertEquals('NO HABIDO', $company->condicion);
+        $this->assertEquals('SUSPENSION TEMPORAL', $company->estado);
+        $this->assertNotEmpty($company->direccion);
+        $this->assertNotEmpty($company->departamento);
+        $this->assertNotEmpty($company->provincia);
+        $this->assertNotEmpty($company->distrito);
+        $this->assertNotEmpty($company->fechaInscripcion);
+        $this->assertNotEmpty($person->dni);
+        $this->assertNotEmpty($person->nombres);
+        $this->assertStringContainsString('NOMBRES', $person->nombres);
+    }
+}
---
+++ tests/Stub/ClientStubDecorator.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Stub;
+
+use Peru\Http\ClientInterface;
+
+/**
+ * Class ClientStubDecorator.
+ *
+ * Override base url with mock url.
+ */
+class ClientStubDecorator implements ClientInterface
+{
+    /**
+     * @var ClientInterface
+     */
+    private $client;
+
+    /**
+     * ClientStubDecorator constructor.
+     *
+     * @param ClientInterface $client
+     */
+    public function __construct(ClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Get Request.
+     *
+     * @param string $url
+     * @param array  $headers
+     *
+     * @return string|false
+     */
+    public function get(string $url, array $headers = [])
+    {
+        return $this->client->get(self::getNewUrl($url), $headers);
+    }
+
+    /**
+     * Post Request.
+     *
+     * @param string $url
+     * @param mixed  $data
+     * @param array  $headers
+     *
+     * @return string|false
+     */
+    public function post(string $url, $data, array $headers = [])
+    {
+        return $this->client->post(self::getNewUrl($url), $data, $headers);
+    }
+
+    public static function getNewUrl($url)
+    {
+        $urlBase = $_ENV['MOCK_URL'];
+        if (empty($urlBase)) {
+            return $url;
+        }
+
+        $u = parse_url($url);
+
+        return $urlBase.$u['path'].(isset($u['query']) ? "?$u[query]" : '');
+    }
+}
