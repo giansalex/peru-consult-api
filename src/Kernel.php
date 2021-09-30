f8a19d1b436312554c957feb7650e903709646ba
Giancarlos Salas <giansalex@gmail.com>
2021-09-29 22:21:37 -0500

    Update symfony 5.3
    drift/kernel conflict symfony/runtime

--- src/Kernel.php
+++ src/Kernel.php
@@ -22,8 +22,8 @@ class Kernel extends BaseKernel
             $container->import('../config/services.yaml');
             $container->import('../config/{services}_'.$this->environment.'.yaml');
             $container->import('../config/react/services.yaml');
-        } elseif (is_file($path = \dirname(__DIR__).'/config/services.php')) {
-            (require $path)($container->withPath($path), $this);
+        } else {
+            $container->import('../config/{services}.php');
         }
     }
 
@@ -34,8 +34,8 @@ class Kernel extends BaseKernel
 
         if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
             $routes->import('../config/routes.yaml');
-        } elseif (is_file($path = \dirname(__DIR__).'/config/routes.php')) {
-            (require $path)($routes->withPath($path), $this);
+        } else {
+            $routes->import('../config/{routes}.php');
         }
     }
 
